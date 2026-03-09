1d07bac2d30a171b1c9c17319486aaa1b2fbf963
Sangarananthan-Dev <sangarananthan.p@digibytetechnologies.com>
2026-03-09 16:18:42 +0530

    plugged in speed insights from vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import Providers from "./providers";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata = {
   title: "Meal Gallery",
   description: "SSR meal gallery powered by TheMealDB and React Query",
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="antialiased">
+        <SpeedInsights />
         <Providers>{children}</Providers>
       </body>
     </html>
